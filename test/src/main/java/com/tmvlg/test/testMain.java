e4b69ec4dfe0fa26a93f11be8466ccda99b1ab6f
aifsbei <aifsbei@mail.ru>
2020-02-20 17:14:13 +0300

    expandable panel fixed

---
+++ test/src/main/java/com/tmvlg/test/testMain.java
@@ -0,0 +1,14 @@
+package com.tmvlg.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class testMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_main);
+    }
+}
